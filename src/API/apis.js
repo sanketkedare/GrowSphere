6c32319e4113c6821e4587a3f0c0f74fd03c633e
Sanket Kedare <sanketkedare200@gmail.com>
2025-01-09 02:59:43 +0530

    API Updated

--- src/API/apis.js
+++ src/API/apis.js
@@ -1,6 +1,11 @@
-export const mainAPI = 'http://localhost:3001'
+export const mainAPI = process.env.VITE_APP_API
 
+
+// For User
 export const register = `${mainAPI}/user/register`;
-export const profile = `${mainAPI}/user/profile`;
+export const profile = `${mainAPI}/user/profile`; // Place id for single profile
+
+
+// For GrowSphere
 export const company = `${mainAPI}/company`;
 
