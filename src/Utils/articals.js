8264ba17ac03a904ccef06cd8c734575de595b71
Sanket Kedare <sanketkedare200@gmail.com>
2025-01-17 21:41:36 +0530

    Rephresed Welcome Massage

--- src/Utils/articals.js
+++ src/Utils/articals.js
@@ -1,5 +1,6 @@
 export const articals = [
-  `Develop a web platform to connect investors with entrepreneurs, bridging the gap between funding opportunities and innovative business ideas.`,
-  `Promote direct investments in businesses, fostering collaboration an ensuring improved returns on investment for stakeholders.`,
-  `Provide a space for seamless discussions, proposal sharing, and collaborative decision-making between investors and entrepreneurs.`,
+`Developed a web platform to bridge the gap between investors and entrepreneurs, providing access to funding opportunities for innovative business ideas.`,
+`Promoted direct investments by fostering collaboration and delivering better returns for all stakeholders.`,
+`Created a seamless platform for communication, proposal sharing, and collaborative decision-making between investors and entrepreneurs.`
+
 ];
